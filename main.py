66ee8766fd14eff78500c76f36caa06a43fa28c6
MarsPain <1434579002@qq.com>
2018-08-27 17:34:34 +0800

    根据训练数据获取tfidf值

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pickle
 # from weight_boosting import compute_labels_weights,get_weights_for_current_batch,get_weights_label_as_standard_dict,init_weights_dict
 import gensim
 from gensim.models import KeyedVectors
-from utils import create_dict
+from utils import create_dict, create_data, get_features
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string("ckpt_dir", "ckpt", "checkpoint location for the model")
@@ -26,6 +26,7 @@ tf.app.flags.DEFINE_string("max_pooling_style", 'chunk_max_pooling',
                            "max_pooling_style:max_pooling,k_max_pooling,chunk_max_pooling. default: chunk_max_pooling")
 tf.app.flags.DEFINE_integer("top_k", 3, "value of top k")
 tf.app.flags.DEFINE_string("traning_data_path", "data/atec_nlp_sim_train.csv", "path of traning data.")
+# tf.app.flags.DEFINE_string("traning_data_path", "data/atec_nlp_sim_train_demo.csv", "path of traning data.")
 tf.app.flags.DEFINE_integer("vocab_size", 13422, "maximum vocab size.")  # 80000
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate")  # 0.001
 tf.app.flags.DEFINE_integer("batch_size", 64, "Batch size for training/evaluating.")
@@ -63,20 +64,25 @@ class Main:
         else:   # 重新读取训练数据并创建各个映射字典
             with open(FLAGS.traning_data_path, "r", encoding="utf-8") as data_f:
                 train_data = csv.reader(data_f, delimiter='\t', quotechar='|')
-                self.word_to_index, self.index_to_word, self.label_to_index, self.index_to_label = create_dict(train_data, word_label_dict)
+                self.word_to_index, self.index_to_word, self.label_to_index, self.index_to_label = \
+                    create_dict(train_data)
                 # print(self.word_to_index)
                 with open(word_label_dict, "wb") as dict_f:  # 创建映射字典后进行存储
-                    pickle.dump([self.word_to_index, self.index_to_word, self.label_to_index, self.index_to_label], dict_f)
+                    pickle.dump([self.word_to_index, self.index_to_word, self.label_to_index, self.index_to_label],
+                                dict_f)
         self.vocab_size = len(self.word_to_index)
         self.num_classes = len(self.label_to_index)
 
     def get_data(self):
         train_valid_test = os.path.join(FLAGS.pkl_dir, "train_valid_test.pkl")
-        if os.path.exists(train_valid_test):
+        if os.path.exists(train_valid_test):    # 若train_valid_test已被处理和存储
             with open(train_valid_test, 'rb') as data_f:
                 train_data, valid_data, test_data = pickle.load(data_f)
-        else:
-            pass
+        else:   # 读取数据集并创建训练集、验证集和测试集
+            with open(FLAGS.traning_data_path, "r", encoding="utf-8") as data_f:
+                all_data = csv.reader(data_f, delimiter='\t', quotechar='|')
+                get_features(all_data)
+                create_data(all_data)
 
     def get_batch_data(self):
         pass
@@ -84,3 +90,4 @@ class Main:
 if __name__ == "__main__":
     main = Main()
     main.get_dict()
+    main.get_data()
--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 from collections import Counter
+import os
+import jieba
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def create_dict(data, path):
+def create_dict(data):
     """
     通过训练集创建字符word和label与索引index之间的双向映射字典
-    :param data:被读取的训练数据
-    :param path:存储字典的路径
+    :param data:从原CSV中读取的训练数据，[index,"能不能开花呗老兄","花呗逾期了还能开通",label（0/1）]
     :return:四个dict:word和label与索引index之间的双向映射字典
     """
     word_to_index = {}
@@ -17,15 +19,47 @@ def create_dict(data, path):
     word_to_index["_UNK"] = 1
     index_to_word[1] = "_UNK"
     c_inputs = Counter()    # Counter用于统计字符串里某个字符出现的次数
+    vocab_list = []  # 存储高词频的word及其相应的频数
     for i, row in enumerate(data):
         string_list_1 = list(row[1])
         string_list_2 = list(row[2])
         c_inputs.update(string_list_1)
         c_inputs.update(string_list_2)
-        vocab_list = c_inputs.most_common(20000)  # 找到词频最高的字符并排序，参数对映射字典大小进行限制
+        vocab_list = c_inputs.most_common(20000)  # 参数对word数量进行限制
     for i, word_freq in enumerate(vocab_list):
         # print(word_freq)  # word_freq是word和相应词频的元组
         word, _ = word_freq
         word_to_index[word] = i + 2
         index_to_word[i+2] = word
     return word_to_index, index_to_word, label_to_index, index_to_label
+
+
+def create_data(data):
+    pass
+
+
+def get_features(data):  # 特征工程，创建和获取各个类型的特征：tfidf值、词向量、编辑距离等
+    tfidf_path = "data/tfidf.txt"    # 存储tfidf值的文件路径
+    if not os.path.exists(tfidf_path):
+        get_tfidf_and_save(data, tfidf_path)
+
+
+def get_tfidf_and_save(data, tfidf_path):
+    corpus = []  # 用于生成tfidf值的语料库
+    for i, row in enumerate(data):
+        str1 = " ".join(jieba.cut(row[1]))
+        str2 = " ".join(jieba.cut(row[2]))
+        corpus.append(str1)
+        corpus.append(str2)
+    # print("string_list:", string_list)
+    tfidfvectorizer = TfidfVectorizer()
+    tfidf = tfidfvectorizer.fit_transform(corpus)
+    # print(tfidf)
+    tfidf = tfidfvectorizer.idf_
+    # print(tfidf)
+    word_to_tfidf = dict(zip(tfidfvectorizer.get_feature_names(), tfidf))
+    # print(word_to_tfidf)
+    with open(tfidf_path, "w", encoding="utf-8") as f:
+        for word, tfidf_score in word_to_tfidf.items():
+            # print(k)
+            f.write(word+"|||"+str(tfidf_score)+"\n")
